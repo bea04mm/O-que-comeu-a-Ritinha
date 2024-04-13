022429977756df295e96a740ab66bcd8bfc0434b
bea04mm <135232707+bea04mm@users.noreply.github.com>
2024-04-14 00:34:26 +0100

    Criação dos Projetos

---
+++ Servidor/O-que-comeu-a-Ritinha/O-que-comeu-a-Ritinha/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace O_que_comeu_a_Ritinha.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
