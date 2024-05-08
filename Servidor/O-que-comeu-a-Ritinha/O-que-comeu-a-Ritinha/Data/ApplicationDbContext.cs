20b01af8473a51327ae4d69a405a4df37ee7e839
bea04mm <135232707+bea04mm@users.noreply.github.com>
2024-05-08 09:47:25 +0100

    Criação da base de dados e migrações

---
+++ Servidor/O-que-comeu-a-Ritinha/O-que-comeu-a-Ritinha/Data/ApplicationDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using O_que_comeu_a_Ritinha.Models;
+
+namespace O_que_comeu_a_Ritinha.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        //public DbSet<Blog> Blog { get; set; }
+        //public DbSet<Books> Books { get; set; }
+        //public DbSet<Comments> Comments { get; set; }
+        //public DbSet<Favoriteorbuy> Favoriteorbuy { get; set; }
+        public DbSet<Ingredients> Ingredients { get; set; }
+        //public DbSet<Likes> Likes { get; set; }
+        //public DbSet<Posts> Posts { get; set; }
+        public DbSet<Recipes> Recipes { get; set; }
+        //public DbSet<Reviews> Reviews { get; set; }
+        public DbSet<Tags> Tags { get; set; }
+        //public DbSet<Utilizadores> Utilizadores { get; set; }
+    }
+}
