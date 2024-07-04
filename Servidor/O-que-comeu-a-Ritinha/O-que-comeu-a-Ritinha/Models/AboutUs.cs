f3415fc24764b69c68cf49eca2c602bebadb15da
bea04mm <135232707+bea04mm@users.noreply.github.com>
2024-07-04 18:07:19 +0100

    Criação da class Aboutus

---
+++ Servidor/O-que-comeu-a-Ritinha/O-que-comeu-a-Ritinha/Models/AboutUs.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace O_que_comeu_a_Ritinha.Models
+{
+    public class Aboutus
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Descrição para Acerca de Nós")]
+        [Required(ErrorMessage = "Este campo é de preenchimento obrigatório.")]
+        public string Description { get; set; }
+
+        [Display(Name = "Imagem para Acerca de Nós")]
+        public string ImageDescription { get; set; }
+
+        [Display(Name = "Imagem para Logo")]
+        public string ImageLogo { get; set; }
+
+        [Display(Name = "Imagem para Cabeçalho")]
+        public string ImageHeader { get; set; }
+
+        [Display(Name = "Imagem para Perfil Normal")]
+        public string ProfileNormal { get; set; }
+
+        [Display(Name = "Imagem para Perfil Selecionado")]
+        public string ProfileSelected { get; set; }
+
+        [Display(Name = "Imagem para Favoritos")]
+        public string Favorites { get; set; }
+
+        [Display(Name = "Imagem para não Favoritos")]
+        public string UnFavorites { get; set; }
+
+        [Display(Name = "Imagem para Facebook")]
+        public string ImageFacebook { get; set; }
+
+        [Display(Name = "Link para Facebook")]
+        [Required(ErrorMessage = "Este campo é de preenchimento obrigatório.")]
+        public string LinkFacebook { get; set; }
+
+        [Display(Name = "Imagem para Instagram")]
+        public string ImageInstagram { get; set; }
+
+        [Display(Name = "Link para Instagram")]
+        [Required(ErrorMessage = "Este campo é de preenchimento obrigatório.")]
+        public string LinkInstagram { get; set; }
+
+        [Display(Name = "Imagem para Youtube")]
+        public string ImageYoutube { get; set; }
+
+        [Display(Name = "Link para Youtube")]
+        [Required(ErrorMessage = "Este campo é de preenchimento obrigatório.")]
+        public string LinkYoutube { get; set; }
+    }
+}
--- Servidor/O-que-comeu-a-Ritinha/O-que-comeu-a-Ritinha/obj/Debug/net8.0/O-que-comeu-a-Ritinha.AssemblyInfo.cs
+++ Servidor/O-que-comeu-a-Ritinha/O-que-comeu-a-Ritinha/obj/Debug/net8.0/O-que-comeu-a-Ritinha.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("O-que-comeu-a-Ritinha")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+232f45811dc0f8b18b5731c25c4ee893d2583c64")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8a4fc706ef64b66101d064412b8804090c98e96")]
 [assembly: System.Reflection.AssemblyProductAttribute("O-que-comeu-a-Ritinha")]
 [assembly: System.Reflection.AssemblyTitleAttribute("O-que-comeu-a-Ritinha")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
